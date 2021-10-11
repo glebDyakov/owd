c81b4e13402f8b59d5d31e1c1a75f2b5f8061d3a
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-10-11 14:49:27 +0300

    addeffects

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +21,8 @@ namespace documenter
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Style dropShadowScrollViewerStyle = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -53,6 +56,82 @@ namespace documenter
         private void textDecorationForegroundHandler(object sender, RoutedEventArgs e)
         {
             fontWeightBolder.Foreground = Brushes.Red;
+        }
+
+        private void fontSizeHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontSize = 34;
+        }
+
+        private void upperCaseHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Text = fontWeightBolder.Text.ToUpper();
+        }
+
+        private void lowerCaseHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Text = fontWeightBolder.Text.ToLower();
+        }
+
+        private void textShadowHandler(object sender, RoutedEventArgs e)
+        {
+            Setter effectSetter = new Setter();
+            effectSetter.Property = ScrollViewer.EffectProperty;
+            effectSetter.Value = new DropShadowEffect
+            {
+                ShadowDepth = 4,
+                Direction = 330,
+                Color = Colors.Black,
+                Opacity = 0.5,
+                BlurRadius = 4
+            };
+            Style dropShadowScrollViewerStyle = new Style(typeof(ScrollViewer));
+            dropShadowScrollViewerStyle.Setters.Add(effectSetter);
+            fontWeightBolder.Resources.Add(typeof(ScrollViewer), dropShadowScrollViewerStyle);
+        }
+
+        private void textSubscriptHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Typography.Variants = FontVariants.Subscript;
+        }
+
+        private void textSuperscriptHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Typography.Variants = FontVariants.Superscript;
+        }
+
+        private void clearStylesHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Typography.Variants = FontVariants.Normal;
+            fontWeightBolder.FontSize = 14;
+            fontWeightBolder.FontWeight = FontWeights.Normal;
+            fontWeightBolder.FontStyle = FontStyles.Normal;
+            fontWeightBolder.TextDecorations = null;
+            fontWeightBolder.Background = Brushes.Transparent;
+            fontWeightBolder.Foreground = Brushes.Black;
+            fontWeightBolder.FontFamily = new FontFamily("Times New Roman");
+            fontWeightBolder.Resources.Clear();
+
+        }
+
+        private void fontFamilyChangeToTimesNewRoman(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontFamily = new FontFamily("Times New Roman");
+        }
+
+        private void fontFamilyChangeToArial(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontFamily = new FontFamily("Arial");
+        }
+
+        private void fontFamilyChangeToCalibri(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontFamily = new FontFamily("Calibri");
+        }
+
+        private void fontFamilyChangeToVerdana(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontFamily = new FontFamily("Verdana");
         }
     }
 }
