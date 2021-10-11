8d18bb603ffee4436b1ab378fcccd2b5e6e039ff
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-10-11 12:04:27 +0300

    addstyles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace documenter
         {
             InitializeComponent();
         }
+
+        private void fontWeightBolderHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontWeight = FontWeights.Bold;
+        }
+
+        private void fontStyleItalicHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.FontStyle = FontStyles.Italic;
+        }
+
+        private void textDecorationUnderlineHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void textDecorationStrikethroughHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.TextDecorations = TextDecorations.Strikethrough;
+        }
+
+        private void textDecorationBackgroundHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Background = Brushes.Red;
+        }
+
+        private void textDecorationForegroundHandler(object sender, RoutedEventArgs e)
+        {
+            fontWeightBolder.Foreground = Brushes.Red;
+        }
     }
 }
